f2eb2646c96f0d72af98fb4be1b89c794a4e82ab
HamzaWebPro <hamza.mern2201@gmail.com>
2023-07-10 19:51:47 +0600

    first commit

--- src/pages/route.js
+++ src/pages/route.js
@@ -14,7 +14,7 @@ import ContactUs from './ContactUs';
 
 function Index(){
 	return(
-		<BrowserRouter basename="/frontend/react">
+		<BrowserRouter>
 			<div className="page-wraper">
 					<Header />
 					<Routes>
