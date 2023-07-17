e7e4d69556935d47b06d8fd6c45e9e73ab02bc74
HamzaWebPro <hamza.mern2201@gmail.com>
2023-07-17 17:47:21 +0600

    faq and progress section added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Dropdown } from "react-bootstrap";
 
@@ -7,6 +7,7 @@ import BannerCard from "./../components/Home/BannerCard";
 import OneStop from "./../components/Home/OneStop";
 import RecentNews from "./../components/Home/RecentNews";
 
+import Accordion from "react-bootstrap/Accordion";
 //images
 import baner1 from "./../assets/images/home-banner/home-icon-left.png";
 import baner2 from "./../assets/images/home-banner/home-icon-right.png";
@@ -29,6 +30,7 @@ import about1 from "./../assets/images/about/about-1.jpg";
 
 import { Parallax, useParallax } from "react-scroll-parallax";
 import FecoTradeEcoSystem from "../components/Home/FecoTradeEcoSystem";
+import { BsArrowDownCircle } from "react-icons/bs";
 
 const trustBlog = [
   {
@@ -74,6 +76,69 @@ function Home() {
     easing: "easeOutQuad",
     translateX: [-40, 20],
   });
+
+  // accordion faq data
+
+  const accordionData = [
+    {
+      question: "What is Bitcoin?",
+      answer:
+        "Bitcoin is a decentralized digital currency that allows for secure, peer-to-peer transactions on the internet. It was created in 2009 by an unknown person or group of people using the name Satoshi Nakamoto.",
+    },
+    {
+      question: "How does Bitcoin work?",
+      answer:
+        "Bitcoin works on a technology called blockchain, which is a distributed ledger that records all transactions across a network of computers. When a transaction is made, it is added to a block and verified by miners. Once verified, the block is added to the blockchain, making the transaction permanent and transparent.",
+    },
+    // Add more questions and answers here...
+  ];
+
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleAccordion = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
+  // count down timer functionality
+  const [targetDate, setTargetDate] = useState(new Date("2023-09-17T23:59:59"));
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  function calculateTimeLeft() {
+    const difference = targetDate.getTime() - new Date().getTime();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+  }
+
+  const { days, hours, minutes, seconds } = timeLeft;
+
+  const totalSeconds =
+    days * 24 * 60 * 60 + hours * 60 * 60 + minutes * 60 + seconds;
+  
+    const progressBarWidth = `${ (((totalSeconds / (60 * 60 * 24)) * 100).toFixed(2))/100}%`;
+
+  console.log(progressBarWidth,"kk");
   return (
     <>
       <div className="page-content">
@@ -87,8 +152,8 @@ function Home() {
 
                 <p className="text text-primary col-lg-6 mx-auto">
                   Fecotrade is one of the largest cryptocurrency trading
-                  platform in the world who offers a robust selection of
-                  trading options and features.
+                  platform in the world who offers a robust selection of trading
+                  options and features.
                 </p>
 
                 <Link
@@ -141,15 +206,30 @@ function Home() {
               </div>
               <div className="col-lg-6 about-content ps-lg-5 m-b30">
                 <div className="section-head">
-                  <h2 className="title">Leading Platform for Cryptocurrency Mining!</h2>
+                  <h2 className="title">
+                    Leading Platform for Cryptocurrency Mining!
+                  </h2>
                   <p className="m-0 lh-base">
-                    Fecotrade is affordable and easy-to-use cloud mining service. We have the cheapest source of renewable electricity in the world. If you’re looking to mine Crypto asset, We are a great choice, allowing you to mine cryptocurrencies without owning any equipment.
+                    Fecotrade is affordable and easy-to-use cloud mining
+                    service. We have the cheapest source of renewable
+                    electricity in the world. If you’re looking to mine Crypto
+                    asset, We are a great choice, allowing you to mine
+                    cryptocurrencies without owning any equipment.
                   </p>
                   <p>
-                    Fectotrade is a Russian - registered fintech company with an extensive ecosystem of crypto asset management services. The main activity of the company are cryptocurrency mining, staking and trading. Our team consists of professional traders with real experience and high profit rates. Unique trading strategies allow our clients to receive high stable profits against their funds.
+                    Fectotrade is a Russian - registered fintech company with an
+                    extensive ecosystem of crypto asset management services. The
+                    main activity of the company are cryptocurrency mining,
+                    staking and trading. Our team consists of professional
+                    traders with real experience and high profit rates. Unique
+                    trading strategies allow our clients to receive high stable
+                    profits against their funds.
                   </p>
                 </div>
-                <Link to={"https://auth.fecotrade.com/register"} className="btn btn-lg btn-primary btn-shadow text-uppercase">
+                <Link
+                  to={"https://auth.fecotrade.com/register"}
+                  className="btn btn-lg btn-primary btn-shadow text-uppercase"
+                >
                   Start Mining Today
                 </Link>
               </div>
@@ -166,8 +246,8 @@ function Home() {
               <div className="section-head text-center">
                 <h2 className="title">Best Option for You</h2>
                 <p className="">
-                  Trust comes from experience. Many of the pleased customers
-                  may function as a guide for you.
+                  Trust comes from experience. Many of the pleased customers may
+                  function as a guide for you.
                 </p>
               </div>
               <div className="row" ref={parallax.ref}>
@@ -238,9 +318,7 @@ function Home() {
                               <img src={coin4} alt="" /> Bitcoin
                             </Dropdown.Item>
                             <Dropdown.Item
-                              onClick={() =>
-                                setSelectText([coin3, "Ethereum"])
-                              }
+                              onClick={() => setSelectText([coin3, "Ethereum"])}
                             >
                               <img src={coin3} alt="" /> Ethereum
                             </Dropdown.Item>
@@ -338,6 +416,108 @@ function Home() {
               </div>
             </div>
           </div>
+        </section>
+        <section className="clearfix section-wrapper1 bg-primary-light">
+          <div className="container">
+            <div className="content-inner-1">
+              <div className="section-head text-center">
+                <h2 className="title">FAQ</h2>
+                <p className="">
+                  Frequently Asked Questions: Get answers to commonly asked
+                  questions about our platform and services.
+                </p>
+              </div>
+              <div className="">
+                {accordionData.map((data, index) => (
+                  <div
+                    className={`accordion-item ${
+                      activeIndex === index ? "active" : ""
+                    }`}
+                    key={index}
+                  >
+                    <div
+                      className="accordion-title"
+                      onClick={() => handleAccordion(index)}
+                    >
+                      <h3>{data.question}</h3>
+                      <span
+                        className={`accordion-icon ${
+                          activeIndex === index ? "active" : ""
+                        }`}
+                      ></span>
+                    </div>
+                    {activeIndex === index && (
+                      <div className="accordion-content">
+                        <p>{data.answer}</p>
+                      </div>
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="content-inner about-sec bg-primary-light">
+          <div className="container">
+            <div className="row about-bx2 style-1 align-items-center">
+              <div className="col-lg-6">
+                <div className="time-container d-flex justify-content-between mb-5">
+                  <div className="time d-flex flex-column justify-content-center align-items-center mb-0">
+                    <h3 className="value mb-0">{formatTime(days)}</h3>
+                    <div className="label">Days</div>
+                  </div>
+                  <div className="time d-flex flex-column justify-content-center align-items-center mb-0">
+                    <h3 className="value mb-0">{formatTime(hours)}</h3>
+                    <span className="label">Hours</span>
+                  </div>
+                  <div className="time d-flex flex-column justify-content-center align-items-center mb-0">
+                    <h3 className="value mb-0">{formatTime(minutes)}</h3>
+                    <span className="label">Minutes</span>
+                  </div>
+                  <div className="time d-flex flex-column justify-content-center align-items-center mb-0">
+                    <h3 className="value mb-0">{formatTime(seconds)}</h3>
+                    <span className="label">Seconds</span>
+                  </div>
+                </div>
+                <div className="progress-bar">
+                <div className="progress" style={{ width: progressBarWidth }}></div>
+                </div>
+              </div>
+              <div className="col-lg-6 about-content ps-lg-5 m-b30">
+                <div className="section-head">
+                  <h2 className="title">
+                    Leading Platform for Cryptocurrency Mining!
+                  </h2>
+                  <p className="m-0 lh-base">
+                    Fecotrade is affordable and easy-to-use cloud mining
+                    service. We have the cheapest source of renewable
+                    electricity in the world. If you’re looking to mine Crypto
+                    asset, We are a great choice, allowing you to mine
+                    cryptocurrencies without owning any equipment.
+                  </p>
+                  <p>
+                    Fectotrade is a Russian - registered fintech company with an
+                    extensive ecosystem of crypto asset management services. The
+                    main activity of the company are cryptocurrency mining,
+                    staking and trading. Our team consists of professional
+                    traders with real experience and high profit rates. Unique
+                    trading strategies allow our clients to receive high stable
+                    profits against their funds.
+                  </p>
+                </div>
+                <Link
+                  to={"https://auth.fecotrade.com/register"}
+                  className="btn btn-lg btn-primary btn-shadow text-uppercase"
+                >
+                  Start Mining Today
+                </Link>
+              </div>
+            </div>
+          </div>
+          <img className="bg-shape1" src={Shape1} alt="" />
+          <img className="bg-shape2" src={Shape3} alt="" />
+          <img className="bg-shape3" src={Shape3} alt="" />
+          <img className="bg-shape4" src={Shape3} alt="" />
         </section>
       </div>
     </>
