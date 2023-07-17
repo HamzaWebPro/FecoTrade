d126444e5bf3d7dbe01c5d7b46d71727cf43a7db
Alen Gomez <81427082+alenpragma@users.noreply.github.com>
2023-07-18 00:32:39 +0600

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -101,6 +101,21 @@ function Home() {
       answer:
         "Effortlessly increase your earnings with Fecotrade's autorun and smart CPU controller. Our application is easy to install and will run automatically using your computer's free CPU resources to generate profits.",
     },
+    {
+      question: "How you confirm the safety of user account?",
+      answer:
+        "Your funds and account are secured with our advanced security features. No one can access your newly mined bitcoins except you.",
+    },
+      {
+      question: "Do you use advance hardware for mining?",
+      answer:
+        "We are using the latest hardware like ASIC Miner & Mining rigs to ensure a high-speed mining experience in a cost-effective manner",
+    },
+        {
+      question: "Can I have more than one account?",
+      answer:
+        "No, Absolutely Not! Since We give Generation Commission and Many Bonuses for all of our users, You can not register more than ONE account with One email and One Device even for your family. If we find more than one account for a user, We will block all of that user accounts without any warning.",
+    },
     // Add more questions and answers here...
   ];
 
