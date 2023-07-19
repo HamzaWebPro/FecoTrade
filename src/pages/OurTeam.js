5fc09b3bb8856a232bf961f8dbe923c3115393a4
Alen Gomez <81427082+alenpragma@users.noreply.github.com>
2023-07-19 23:57:00 +0600

    Update OurTeam.js

--- src/pages/OurTeam.js
+++ src/pages/OurTeam.js
@@ -3,12 +3,12 @@
 const personImageOne = "https://i.ibb.co/LSYS36V/male-man-icon.png"
 
 const personData = [
-    { image: personImageOne, name: "PC Daniel", title: "FOUNDER & CEO", description: "PC is our passionate CEO and leads Fecotrade Ecosystem for the revoulationary change." },
-    { image: personImageOne, name: "Shang Cheng", title: "Co-Founder", description: "Our core team leader is Shang Cheng and he has the pioneer knowledge about crypto mining." },
-    { image: personImageOne, name: "Nash Josef", title: "Cheif Technical Officer", description: "Nash Josef is one of the best CTO of the world. He leads Feco Technically." },
-    { image: personImageOne, name: "Criss Jordan", title: "Cheif Marketing Officer", description: "Criss is our Cheif Technical Officer. He leads the core marketing team." },
-    { image: personImageOne, name: "Rosario Lion", title: "Operations Manager", description: "Rosario Lion is our Operations Manager. She works for the operational management." },
-    { image: personImageOne, name: "Alex Sam", title: "Finance Manager", description: "Finance Manager is a Finance and Administration expert. He is one of the best team member of Fecotrade." },
+    { image: personImageOne, name: "Novel Polyakov", title: "FOUNDER & CEO", description: "Novel Polyakov is our passionate CEO and leads Fecotrade Ecosystem for the revoulationary change." },
+    { image: personImageOne, name: "Samson Zhdanov", title: "Co-Founder", description: "Our core team leader is Samson Zhdanov and he has the pioneer knowledge about crypto mining." },
+    { image: personImageOne, name: "Nikon Abramowitz", title: "Cheif Technical Officer", description: "Nikon Abramowitz is one of the best CTO of the world. He leads Feco Technically." },
+    { image: personImageOne, name: "Herman Andreeff", title: "Cheif Marketing Officer", description: "Herman Andreeff is our Cheif Technical Officer. He leads the core marketing team." },
+    { image: personImageOne, name: "Tatiana Cherkasova", title: "Operations Manager", description: "Tatiana Cherkasova is our Operations Manager. She works for the operational management." },
+    { image: personImageOne, name: "Ahmed Baryshnikov", title: "Finance Manager", description: "Ahmed Baryshnikov is a Finance and Administration expert. He is one of the best team member of Fecotrade." },
 ]
 
 
