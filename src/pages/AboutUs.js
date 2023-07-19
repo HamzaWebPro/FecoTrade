16216becacbcc52e548cc5b720398d03d0e0fa68
Remon Tripura <remontripura045@gmail.com>
2023-07-19 14:24:10 +0600

    about us section changes

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,5 +1,5 @@
-import React,{useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import ModalVideo from 'react-modal-video'
 
 import PageLayout from './../layouts/PageLayout';
@@ -8,7 +8,7 @@ import RecentNews from './../components/Home/RecentNews';
 
 //Images
 import Shape1 from './../assets/images/home-banner/shape1.png';
-import Shape3 from './../assets/images/home-banner/shape3.png'; 
+import Shape3 from './../assets/images/home-banner/shape3.png';
 
 import about2 from './../assets/images/about/feco-about-1.jpg';
 import about3 from './../assets/images/about/feco-about-2.jpg';
@@ -19,42 +19,43 @@ import videobox from './../assets/images/about/videobx.png';
 
 import bloglg from './../assets/images/blog/blog-ig.png';
 import avatar3 from './../assets/images/avatar/avatar3.jpg';
+import OurTeam from './OurTeam';
 
-const ImageBox = ({image, changeClass}) =>{
-    return(
-        
+const ImageBox = ({ image, changeClass }) => {
+    return (
+
         <div className="col-6">
             <div className={`image-box ${changeClass}`}>
                 <img src={image} alt="" />
             </div>
         </div>
-        
+
     )
 }
 
-function AboutUs(){
+function AboutUs() {
     const nav = useNavigate();
     const formDetails = (e) => {
         e.preventDefault();
         nav("/contact-us");
     };
     const [isOpen, setOpen] = useState(false)
-    return(
+    return (
         <>
             <div className="page-content">
-                <PageLayout pageTitle="About Us" />               
+                <PageLayout pageTitle="About Us" />
                 <section className="content-inner about-sec bg-primary-light">
                     <div className="container">
                         <div className="row about-bx2 style-1 align-items-center">
                             <div className="col-lg-6">
                                 <div className="dz-media">
-                                    <div className="row align-items-end">                                        
+                                    <div className="row align-items-end">
                                         <ImageBox image={about4} changeClass="image-box-1" />
-                                        <ImageBox image={about2} changeClass="image-box-2" />                                      
+                                        <ImageBox image={about2} changeClass="image-box-2" />
                                     </div>
-                                    <div className="row">                                        
+                                    <div className="row">
                                         <ImageBox image={about3} changeClass="image-box-3" />
-                                        <ImageBox image={about1} changeClass="image-box-4" />                                        
+                                        <ImageBox image={about1} changeClass="image-box-4" />
                                     </div>
                                 </div>
                             </div>
@@ -66,11 +67,11 @@ function AboutUs(){
                                 <Link to={"https://auth.fecotrade.com/register"} className="btn btn-lg btn-primary btn-shadow text-uppercase">Start Mining</Link>
                             </div>
                         </div>
-                    </div>                    
+                    </div>
                     <img className="bg-shape1" src={Shape1} alt="" />
-					<img className="bg-shape2" src={Shape3} alt="" />
-					<img className="bg-shape3" src={Shape3} alt="" />
-					<img className="bg-shape4" src={Shape3} alt="" />
+                    <img className="bg-shape2" src={Shape3} alt="" />
+                    <img className="bg-shape3" src={Shape3} alt="" />
+                    <img className="bg-shape4" src={Shape3} alt="" />
                 </section>
                 <section className="content-inner p-0 bg-primary-light video-bx-wrapper">
                     <img className="bg-shape1" src={Shape1} alt="" />
@@ -78,16 +79,27 @@ function AboutUs(){
                         <div className="video-bx style-1">
                             <div className="video-media">
                                 <img src={videobox} alt="" />
-                                <Link to={"#"} className="popup-youtube play-icon" onClick={()=> setOpen(true)}>
+                                <Link to={"#"} className="popup-youtube play-icon" onClick={() => setOpen(true)}>
                                     <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M6.25 3.75L23.75 15L6.25 26.25V3.75Z" stroke="#9467FE" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round"/>
+                                        <path d="M6.25 3.75L23.75 15L6.25 26.25V3.75Z" stroke="#9467FE" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
                                     </svg>
                                 </Link>
                             </div>
                         </div>
                     </div>
                 </section>
-                <section className="content-inner bg-light pricing-plan-wrapper2">
+                <section className="content-inner bg-light icon-section section-wrapper2">
+                    <div className="container">
+                        <div className="section-head text-center">
+                            <h2 className="title">Our Professional Team</h2>
+                            <p>we are proud of our great team. He is one of the most motivated and enthusiastic people we have, and is always ready and willing to help out where needed.</p>
+                        </div>
+                        <div className="row sp60">
+                            <OurTeam />
+                        </div>
+                    </div>
+                </section>
+                {/* <section className="content-inner bg-light pricing-plan-wrapper2">
                     <img className="bg-shape2" src={Shape1} alt="" />
                     <div className="container">
                         <div className="section-head text-center">
@@ -96,8 +108,8 @@ function AboutUs(){
                         <div className="row justify-content-center">
                             <PriceBlog />
                         </div>
-                    </div>    
-                </section>
+                    </div>
+                </section> */}
                 <section className="form-sec bg-light">
                     <img className="bg-shape2" src={Shape1} alt="" />
 
@@ -111,10 +123,10 @@ function AboutUs(){
                                     </div>
                                 </div>
                                 <div className="col-xl-9">
-                                    <form className="dzForm"  onSubmit={(e) => formDetails(e)}>
+                                    <form className="dzForm" onSubmit={(e) => formDetails(e)}>
                                         <div className="dzFormMsg"></div>
-                                            <input type="hidden" className="form-control" name="dzToDo" value="Contact" />
-                                            <input type="hidden" className="form-control" name="reCaptchaEnable" value="0" />
+                                        <input type="hidden" className="form-control" name="dzToDo" value="Contact" />
+                                        <input type="hidden" className="form-control" name="reCaptchaEnable" value="0" />
 
                                         <div className="row">
                                             <div className="col-md-4 col-sm-6 m-b30 wow fadeInUp" data-wow-delay="0.1s">
@@ -145,39 +157,39 @@ function AboutUs(){
                 <section className="content-inner bg-white blog-wrapper">
                     <img className="bg-shape1" src={Shape1} alt="" />
                     <div className="container">
-						<div className="row">
-							<div className="col-xl-7 col-lg-12">
-								<div className="section-head " >
-									<h6 className="sub-title text-primary">FROM OUR BLOG</h6>
-									<h2 className="title">Recent News &amp; Updates</h2>
-								</div>
-								<RecentNews />
-							</div>
-							<div className="col-xl-5 col-lg-12 m-b30 " >
-								<div className="dz-card style-2" style={{backgroundImage: "url("+ bloglg +")"}}>
-									<div className="dz-category">
-										<ul className="dz-badge-list">
-											<li><Link to={"#"} className="dz-badge">14 Fan 2022</Link></li>
-										</ul>
-									</div>
-									<div className="dz-info">
-										<h2 className="dz-title"><Link to={"/blog-details"} className="text-white">Directly support individuals Crypto</Link></h2>
-										<div className="dz-meta">
-											<ul>
-												<li className="post-author">
-													<Link to={"#"}>
-														<img src={avatar3} alt=""  className="me-2"/>
-														<span>By Noare</span>
-													</Link>
-												</li>
-												<li className="post-date"><Link to={"#"}> 12 May 2022</Link></li>
-											</ul>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+                        <div className="row">
+                            <div className="col-xl-7 col-lg-12">
+                                <div className="section-head " >
+                                    <h6 className="sub-title text-primary">FROM OUR BLOG</h6>
+                                    <h2 className="title">Recent News &amp; Updates</h2>
+                                </div>
+                                <RecentNews />
+                            </div>
+                            <div className="col-xl-5 col-lg-12 m-b30 " >
+                                <div className="dz-card style-2" style={{ backgroundImage: "url(" + bloglg + ")" }}>
+                                    <div className="dz-category">
+                                        <ul className="dz-badge-list">
+                                            <li><Link to={"#"} className="dz-badge">14 Fan 2022</Link></li>
+                                        </ul>
+                                    </div>
+                                    <div className="dz-info">
+                                        <h2 className="dz-title"><Link to={"/blog-details"} className="text-white">Directly support individuals Crypto</Link></h2>
+                                        <div className="dz-meta">
+                                            <ul>
+                                                <li className="post-author">
+                                                    <Link to={"#"}>
+                                                        <img src={avatar3} alt="" className="me-2" />
+                                                        <span>By Noare</span>
+                                                    </Link>
+                                                </li>
+                                                <li className="post-date"><Link to={"#"}> 12 May 2022</Link></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </div>
             <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId="cfmQFW1DpA0" onClose={() => setOpen(false)} />
