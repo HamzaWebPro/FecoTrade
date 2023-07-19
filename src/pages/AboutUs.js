1a180cbbdb863daad6c4ff97734dd915a6a0db8e
Alen Gomez <81427082+alenpragma@users.noreply.github.com>
2023-07-19 18:26:47 +0600

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -61,10 +61,14 @@ function AboutUs() {
                             </div>
                             <div className="col-lg-6 about-content ps-lg-5 m-b30">
                                 <div className="section-head">
-                                    <h2 className="title">Leading Platform for Cryptocurrency Mining!</h2>
+                                    <h2 className="title">Best Platform for Crypto Trading, Mining and Staking!</h2>
                                     <p className="m-0 lh-base">Fecotrade is affordable and easy-to-use cloud mining service. We have the cheapest source of renewable electricity in the world. If you’re looking to mine Crypto asset, We are a great choice, allowing you to mine cryptocurrencies without owning any equipment.
 
-                                Fectotrade is a Russian - registered fintech company with an extensive ecosystem of crypto asset management services. The main activity of the company are cryptocurrency mining, staking and trading. Our team consists of professional traders with real experience and high profit rates. Unique trading strategies allow our clients to receive high stable profits against their funds.</p>
+                                Fectotrade is a Russian - registered fintech company with an extensive ecosystem of crypto asset management services. The main activity of the company are cryptocurrency mining, staking and trading. Our team consists of professional traders with real experience and high profit rates. 
+        Unique trading strategies allow our clients to receive high stable profits against their funds.</p>
+                                <h2>Our Mission</p>
+        <p>Fectotrade is a Russian - registered fintech company with an extensive ecosystem of crypto asset management services. The main activity of the company are cryptocurrency mining, staking and trading. Our team consists of professional traders with real experience and high profit rates. 
+        Unique trading strategies allow our clients to receive high stable profits against their funds.</p>
                                 </div>
                                 <Link to={"https://auth.fecotrade.com/register"} className="btn btn-lg btn-primary btn-shadow text-uppercase">Start Mining</Link>
                             </div>
