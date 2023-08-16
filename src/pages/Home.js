ace8d49c590672cd63d92fe31a32df5645ac36f4
sajid <sajid.mern2201@gmail.com>
2023-08-16 15:42:08 +0600

    cryto widget added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Dropdown } from "react-bootstrap";
+import CryptoWidget from "../components/Home/CryptoWidget"
 
 //components
 import BannerCard from "./../components/Home/BannerCard";
@@ -381,38 +382,7 @@ function Home() {
 
         {/* <img className="bg-shape1" src={Shape1} alt="" /> */}
 
-        {/* CryptoWidget section */}
-        <section className="bg-light">
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="section-head text-center">
-                        <h2 className="title text-[#362465]">Popular Cryptocurrencies</h2>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-4 border__style mt-4">
-                      
-                      <iframe className="border__style" width="100%" height="172" frameBorder="0" scrolling="no" src="https://coinbrain.com/coins/fant-0x321162cd933e2be498cd2267a90534a804051b11/ticker?theme=light&padding=16&type=medium&currency=USD&blocks=price%2CmarketCap%2Cvolume24h"></iframe>
-                    </div>
-                    <div className="col-lg-4 border__style mt-4">
-                      <iframe className="border__style" width="100%" height="172" frameBorder="0" scrolling="no" src="https://coinbrain.com/coins/bnb-0x2170ed0880ac9a755fd29b2688956bd959f933f8/ticker?theme=light&padding=16&type=medium&currency=USD&blocks=price%2CmarketCap%2Cvolume24h"></iframe>
-                    </div>
-                    <div className="col-lg-4 border__style mt-4">
-
-                      <iframe className="border__style" width="100%" height="172" frameBorder="0" scrolling="no" src="https://coinbrain.com/coins/bnb-0xba2ae424d960c26247dd6c32edc70b295c744c43/ticker?theme=light&padding=16&type=medium&currency=USD&blocks=price%2CmarketCap%2Cvolume24h"></iframe>
-                    </div>
-                    <div className="col-lg-4 mt-4">
-                      <iframe className="border__style" width="100%" height="172" frameBorder="0" scrolling="no" src="https://coinbrain.com/coins/bnb-0x1d2f0da169ceb9fc7b3144628db156f3f6c60dbe/ticker?theme=light&padding=16&type=medium&currency=USD&blocks=price%2CmarketCap%2Cvolume24h"></iframe>
-                    </div>
-                    <div className="col-lg-4 mt-4">
-                      <iframe className="border__style" width="100%" height="172" frameBorder="0" scrolling="no" src="https://coinbrain.com/coins/bnb-0x7083609fce4d1d8dc0c979aab8c869ea2c873402/ticker?theme=light&padding=16&type=medium&currency=USD&blocks=price%2CmarketCap%2Cvolume24h"></iframe>
-                    </div>
-                    <div className="col-lg-4 mt-4">
-                      <iframe className="border__style" width="100%" height="172" frameBorder="0" scrolling="no" src="https://coinbrain.com/coins/eth-0xdac17f958d2ee523a2206206994597c13d831ec7/ticker?theme=light&padding=16&type=medium&currency=USD&blocks=price%2CmarketCap%2Cvolume24h"></iframe>
-                    </div>
-                </div>
-            </div>
-        </section>
+        
 
         <section className="content-inner bg-light icon-section section-wrapper2">
           <div className="container">
@@ -483,6 +453,7 @@ function Home() {
           <img className="bg-shape3" src={Shape3} alt="" />
           <img className="bg-shape4" src={Shape3} alt="" /> */}
         </section>
+        <CryptoWidget />
         {/*
         <section className="content-inner bg-light icon-section section-wrapper2">
           <div className="container">
@@ -584,6 +555,7 @@ function Home() {
             </div>
           </div>
         </section> */}
+        
       </div>
     </>
   );
